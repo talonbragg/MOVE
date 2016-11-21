4b4c405895d613036606ed35ba83b24909411655
Talon Bragg <pvpgamecrash@gmail.com>
2016-11-20 18:54:45 -0800

    Update index.js

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ function moveDown() {
 	myElement.style.top = newTop2 + 'px';
 }
 function moveGold() {
-	Math.floor(Math.random() * left) + topStyle;
+	Math.floor(Math.random() * left2) + topStyle2;
 }
