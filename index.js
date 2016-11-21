5b8f631aa27ad6a4efb372cf444e770c8f20246f
Talon Bragg <pvpgamecrash@gmail.com>
2016-11-21 09:55:19 -0800

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function moveGold() {
 	var theLeft = Math.floor(Math.random() * screen.width) + 1;
 	myElement2.style.top = theTop + 'px';
 	myElement2.style.left = theLeft + 'px';
-	setTimeout(arguments.callee, 12000);
+	setTimeout(arguments.callee, 17000);
 		if (myElement.style.top == myElement2.style.top && myElement.style.left == myElement2.style.left) {
 			confirm("You Win!");
 			document.location.reload();
