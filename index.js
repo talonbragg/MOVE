84f99418332a8e53ffe7a60f6a35c313d27967ca
Talon Bragg <pvpgamecrash@gmail.com>
2016-11-20 17:11:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-var myElement = document.getElementById('dot');
-var styles = window.getComputedStyle(myElement);
-var top = parseInt(styles.top);
-var left = parseInt(styles.left);
-function moveLeft() {
-	var newLeft = left - 5;
-	left = newLeft;
-	myElement.style.left = newLeft + 'px';
-}
-function moveUp() {
-	var newTop = top - 5;
-	top = newTop;
-	myElement.style.top = newTop + 'px';
-}
-function moveRight() {
-	var newLeft2 = left + 5;
-	left = newLeft2;
-	myElement.style.left = newLeft2 + 'px';
-}
-function moveDown() {
-	var newTop2 = top + 5;
-	top = newTop2
-	myElement.style.top = newTop2 + 'px';
-}
+(function() {
+	var myElement = document.getElementById('dot');
+	var styles = window.getComputedStyle(myElement);
+	var top = parseInt(styles.top);
+	console.log(styles.top);
+	var left = parseInt(styles.left);
+	function moveLeft() {
+		var newLeft = left - 5;
+		left = newLeft;
+		myElement.style.left = newLeft + 'px';
+	}
+	function moveUp() {
+		var newTop = top - 5;
+		top = newTop;
+		myElement.style.top = newTop + 'px';
+	}
+	function moveRight() {
+		var newLeft2 = left + 5;
+		left = newLeft2;
+		myElement.style.left = newLeft2 + 'px';
+	}
+	function moveDown() {
+		var newTop2 = top + 5;
+		top = newTop2
+		myElement.style.top = newTop2 + 'px';
+	}
+})();
