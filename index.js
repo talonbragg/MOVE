5375a76198cf94e3b697e768bcc0e6090e167ad9
Talon Bragg <pvpgamecrash@gmail.com>
2016-11-20 16:42:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var myElement = document.getElementById('dot');
 var styles = window.getComputedStyle(myElement);
 var top = parseInt(styles.top);
-var left = parsenInt(styles.left);
+var left = parseInt(styles.left);
 function moveLeft() {
 	var newLeft = left - 1;
 	document.getElementById("dot").style.left = newLeft + 'px';
