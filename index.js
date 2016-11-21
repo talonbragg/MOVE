c46f1c22041237e51387864f5e57d6371ff8356b
Talon Bragg <pvpgamecrash@gmail.com>
2016-11-20 18:04:28 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ var styles = window.getComputedStyle(myElement);
 var topStyle = parseInt(styles.top);
 var left = parseInt(styles.left);
 function moveLeft() {
-	var newLeft = left - 5;
+	var newLeft = left - 10;
 	left = newLeft;
 	myElement.style.left = newLeft + 'px';
 }
 function moveUp() {
-	var newTop = topStyle - 5;
+	var newTop = topStyle - 10;
 	topStyle = newTop;
 	myElement.style.top = newTop + 'px';
 }
 function moveRight() {
-	var newLeft2 = left + 5;
+	var newLeft2 = left + 10;
 	left = newLeft2;
 	myElement.style.left = newLeft2 + 'px';
 }
 function moveDown() {
-	var newTop2 = topStyle + 5;
+	var newTop2 = topStyle + 10;
 	topStyle = newTop2
 	myElement.style.top = newTop2 + 'px';
 }
